ad82f119cb9dfc95481c706285ebb846935f6d00
Anfal Eltahir <anfaleltahir93@gmail.com>
2025-08-24 03:39:38 +0400

    msg

--- src/ProjectDetails.jsx
+++ src/ProjectDetails.jsx
@@ -51,7 +51,7 @@ function ProjectDetails() {
           rel="noopener noreferrer"
           className="inline-block mt-4 px-6 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
         >
-          Visit My Shopify Store
+          Visit The Website
         </a>
       )}
       <br /><br />
